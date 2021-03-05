6cf7550ad39022560f8b61c7725dde9a8b260171
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-03-05 21:57:22 +0600

    update 11

--- src/components/Draft/Draft.js
+++ src/components/Draft/Draft.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Draft = (props) => {
     const draft = props.draft;
+if(draft.length<11){
     const totalSalary = draft.reduce((sum, player) => sum + player.salary, 0)
     let playerName;
     for (let i = 0; i < draft.length; i++) {
@@ -44,17 +45,15 @@ const Draft = (props) => {
 </div>
 
                 <li class="list-group-item">Total Budget:$ {totalSalary} </li>
-            
-          
+                  
             </div>
-           
-
-     
-   
-   
         </div>
         
-    );
+    );  
+}
+  else{
+      alert("you can not select more then 11 player")
+  }
 };
 
 export default Draft;
